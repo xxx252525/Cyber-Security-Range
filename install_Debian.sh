2092683b758b2c351f81d83e6d8bdadf53ed5a05
天阁创客official <104115333+xxx252525@users.noreply.github.com>
2024-09-24 15:53:57 +0800

    Update and rename install.sh to install_Debian.sh

--- install_Debian.sh
+++ install_Debian.sh
@@ -11,31 +11,32 @@ execute_command() {
 }
 
 # 安装基础软件包
-execute_command sudo dnf install -y vim curl git wget
+execute_command sudo apt install -y vim curl git wget
 
 # 安装和配置SSH服务
-execute_command sudo dnf install -y openssh-server
+execute_command sudo apt install -y openssh-server
 execute_command sudo systemctl start sshd.service
 execute_command sudo systemctl enable sshd.service
 execute_command sudo cp /etc/ssh/sshd_config /etc/ssh/ssh_config.back
 
 # 安装和配置HTTP服务
-execute_command sudo dnf install -y httpd
+execute_command sudo apt install -y httpd
 execute_command sudo systemctl start httpd.service
 execute_command sudo systemctl enable httpd.service
 
 # 安装和配置MariaDB服务
-execute_command sudo dnf install -y mariadb-server
+execute_command sudo apt install -y mariadb-server
 execute_command sudo systemctl start mariadb
 execute_command sudo mysqladmin -uroot password '123456'
 execute_command sudo systemctl enable mariadb.service
 
 # 安装PHP和相关模块
-execute_command sudo dnf install -y php php-json php-fpm php-gd php-xml
+execute_command sudo apt install -y php php-json php-fpm php-gd php-xml
+execute_command sudo apt install php php-pear php-devel
 php --version
 
 # 安装Java OpenJDK
-execute_command sudo dnf install -y java-21-openjdk
+execute_command sudo apt install -y java-21-openjdk
 java --version
 
 # 停用防火墙和SELinux
@@ -44,7 +45,7 @@ execute_command sudo setenforce 0
 execute_command sudo systemctl disable firewalld.service
 
 # 安装和配置Docker服务
-execute_command sudo dnf install -y docker
+execute_command sudo apt install -y docker
 execute_command sudo systemctl start docker
 execute_command sudo systemctl enable docker
 
